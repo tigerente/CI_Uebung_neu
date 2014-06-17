98ce1e7044d9846db97bda8a376d53b8f6b62fb5
tigerente <janos.sebestyen@web.de>
2014-06-17 17:16:44 +0200

    Aufgabe 6_2 Skript verbessert

--- Blatt06/Aufgabe6_2.m
+++ Blatt06/Aufgabe6_2.m
@@ -1,3 +1,5 @@
+%% I) VORBEREITUNG
+
 %Parameter-Setup
 minVal = -2 * pi;
 maxVal = 2 * pi;
@@ -5,14 +7,9 @@ nrTrain = 60;
 nrValidate = 60;
 nrTest = 200;
 f = @(x) cos(x);
-noise = true;
-alpha = 0.01;
-
-%Verhinderung von Overfitting
-NONE = 0;   % Referenz
-EAST = 1;   % Early Stopping
-REGL = 2;   % Regularisierung
-regRatio = 0.1;
+noise = false;
+alpha = 0.01; % Rauschfaktor
+regRatio = 0.4; % Regularisierungsfaktor
 
 %Validierungsdaten erstellen: linear
 validateX = linspace(minVal,maxVal, nrValidate);
@@ -33,50 +30,82 @@ if (noise == true)
     trainY = trainY + alpha * randn(1,nrTrain);
 end
 
-for antiOverfit = [NONE, EAST, REGL]
-    %MLP mit 25 Neuronen in verdeckter Schicht erstellen
-    net = newff([minVal,maxVal], [-1 1], 25);
-    
-    %Aufteilung der Daten in Training/Validierung/Test
-    net.divideFcn = 'divideind';
-    net.divideParam.trainInd = 1:nrTrain;
-    net.divideParam.valInd = [];
-    if (antiOverfit == EAST) % Validierungsmenge nur bei Early Stopping
-        net.divideParam.valInd = nrTrain+(1:nrValidate);
-    end
-    net.divideParam.testInd = nrTrain+nrValidate+(1:nrTest);
-    
-    %Performance-Function
-    net.performFcn = 'mse';
-    if (antiOverfit == REGL) % Regularisierung
-        net.performParam.regularization = regRatio;
-    end
-    
-    % Abbruchbedingungen:
-    net.trainParam.epochs = 2000;
-    net.trainParam.min_grad = 0;
-    net.trainParam.goal = 0;
-    
-    %Anzeige des Fensters deaktivieren
-    net.trainParam.showWindow = 1;
+%MLP mit 25 Neuronen in verdeckter Schicht erstellen
+net = newff([minVal,maxVal], [-1 1], 25);
 
-    %Training des MLPs
-    [net,tr] = train(net, [trainX validateX testX], [trainY validateY testY]);
+% Daten-Aufteilung
+net.divideFcn = 'divideind';
 
-    %Testfehler merken
-    testError = tr.tperf(end);
-    disp(['Testfehler: ' num2str(testError)])
+% Abbruchbedingungen:
+net.trainParam.epochs = 2000;
+net.trainParam.min_grad = 0;
+net.trainParam.goal = 0;
 
-    %Ausgabe des Netzes bestimmen
-    plotX = linspace (minVal,maxVal,1000);
-    predY = sim(net,plotX);
+%Anzeige des Fensters deaktivieren
+net.trainParam.showWindow = 1;
 
-    %Ausgabe und Zielfunktion plotten
+% Datenreihe fuer Plots
+plotX = linspace (minVal,maxVal,1000);
 
-    figure;
-    plot(trainX, trainY, '.', plotX,f(plotX), plotX, predY)
-    xlabel('Eingabe')
-    ylabel('Ausgabe')
-    legend('Trainingsdaten','Zielfunktion','Fit')
+%% II.1) REFERENZ: Keine Massnahme gegen Overfitting
+% Kopie anlegen
+net1 = net;
+%Aufteilung der Daten in Training/Test
+net1.divideParam.trainInd = 1:nrTrain;
+net1.divideParam.valInd = [];
+net1.divideParam.testInd = nrTrain+(1:nrTest);
 
-end
+%Training des MLPs
+[net1,tr] = train(net1, [trainX testX], [trainY testY]);
+
+%Ausgabe des Netzes bestimmen
+predY = sim(net1,plotX);
+
+%% II.2) Early Stopping
+% Kopie anlegen
+net2 = net;
+%Aufteilung der Daten in Training/Validierung/Test
+net2.divideParam.trainInd = 1:nrTrain;
+net2.divideParam.valInd = nrTrain+(1:nrValidate);
+net2.divideParam.testInd = nrTrain+nrValidate+(1:nrTest);
+
+%Training des MLPs
+[net2,tr] = train(net2, [trainX validateX testX], [trainY validateY testY]);
+
+%Ausgabe des Netzes bestimmen
+predY = sim(net2,plotX);
+
+%% II.3) REGULARISIERUNG
+% Kopie anlegen
+net3 = net;
+%Aufteilung der Daten in Training/Test
+net3.divideParam.trainInd = 1:nrTrain;
+net3.divideParam.valInd = [];
+net3.divideParam.testInd = nrTrain+(1:nrTest);
+
+%Performance-Function
+net3.performFcn = 'mse';
+net3.performParam.regularization = regRatio;
+
+%Training des MLPs
+[net3,tr] = train(net3, [trainX testX], [trainY testY]);
+
+%Ausgabe des Netzes bestimmen
+predY = sim(net3,plotX);
+
+%% III) AUSWERTUNG
+
+%Testfehler plotten und letzten Wert auf der Konsole ausgeben
+plotperf(tr);
+testError = tr.tperf(end);
+trainingsError = tr.perf(end);
+disp(['Testfehler: ' num2str(testError)])
+disp(['Trainingsfehler: ' num2str(trainingsError)])
+
+%Ausgabe und Zielfunktion plotten
+
+figure;
+plot(trainX, trainY, '.', plotX,f(plotX), plotX, predY)
+xlabel('Eingabe')
+ylabel('Ausgabe')
+legend('Trainingsdaten', 'Zielfunktion','Fit')
