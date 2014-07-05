9fe461c2365c8f124d594b04dff455d617ee3f73
tigerente <janos.sebestyen@web.de>
2014-07-05 18:43:01 +0200

    Feinschliff

--- Blatt09/Aufgabe9_2.m
+++ Blatt09/Aufgabe9_2.m
@@ -1,15 +1,24 @@
 % Parameter-Setup:
 C = 3;              % Anzahl der Cluster
-epsilon = 0.1;     % Abbruchgenauigkeit
-R = 25;             % Anzahl an Runs
-disp_det = false;   % Details der Auswertung anzeigen
-
-correct = zeros(R,3);
+epsilon = 0.1;      % Abbruchgenauigkeit
+R = 1;              % Anzahl an Runs (a)
+disp_det = true;    % Details der Auswertung anzeigen (a)
+M = 1.5;            % Unschaerfe (a)
+%R = 100;            % Anzahl an Runs (b)
+%disp_det = false;   % Details der Auswertung anzeigen (b)
+%M = [1.3 1.5 1.7];  % Unschaerfe (b)
 
 clc
-for m = [5.5 6 6.5];   % Unschaerfe
+for m = M
+    correct = zeros(R,3);
+    max_idx = zeros(R,3);
+    doublet = zeros(R,1);
+    
     display(['Unschaerfe: ' num2str(m)])
     for r = 1 : R; % Runs (wegen Zufall)
+        if (disp_det)
+            display(['Run ' num2str(r)])
+        end
         % Daten einlesen:
         iris = csvread('iris.data');
 
@@ -24,7 +33,7 @@ for m = [5.5 6 6.5];   % Unschaerfe
         [~,clusters] = max(U);
 
         % Ergebnis ueberpruefen:
-        art_clusters = cell(1,3);
+        art_clusters = cell(1,3); % Anzahlen der Clusterzuordnungen je Art
         for i = 1:3
             art_clusters{i} = histc(clusters(art_idx{i}), 1:3); 
             if (disp_det == true)
@@ -33,11 +42,16 @@ for m = [5.5 6 6.5];   % Unschaerfe
                 num2str(art_clusters{i}(2)) ' zu Cluster 2 und ' ...
                 num2str(art_clusters{i}(3)) ' zu Cluster 3.'])
             end
-            correct(r, i) = max(art_clusters{i});
+            [correct(r,i), max_idx(r,i)] = max(art_clusters{i});
+        end
+        if (max(histc(max_idx(r,:), 1:3))>1) % doppelt belegte Cluster
+            doublet(r) = 1;
         end
     end
     
-    display(['Insgesamt werden durchschnittlich ' num2str(mean(sum(correct,2)))...
-        ' Individuen korrekt zugeordnet.']);
+    display(['Insgesamt wurden durchschnittlich ' num2str(mean(sum(correct,2)))...
+        ' Individuen einem Hauptcluster zugeordnet.']);
+    display(['Es gab ' num2str(sum(doublet))...
+    ' Faelle von doppelt belegten Hauptclustern.']);
     display(' ');
 end
