c08b18f706679e40b176f5865fcaf6bc710e067b
FrankosLosTankos <frank.mehne@gmx.de>
2014-07-11 12:20:48 +0200

    1 b) angefangen

---
+++ Blatt10/paramNew.m
@@ -0,0 +1,17 @@
+function paramNew = paramNew(x,y,param)
+%PARAMNEW(x,y,param)
+% Berechnet den neuen Parameterverktor nach dem
+% Passive-Aggressive-Algorithmus
+% PARAMETER:
+%   x:      x-Wert im Eingangsraum
+%   y:      Vorgegebener y-Wert, der erreicht werden soll
+%   param:  aktueller Parameterverktor
+%
+% RETURN:
+%   paramNew:   mit Hilfe des Lerndatums (x,y) aktualisierter ParameterVektor
+
+
+
+
+end
+
